b3fb4ae94dede592510453fe49b9894e6d7e3fb8
myungshink67 <myungshink67@gmail.net>
2022-12-13 16:21:12 +0900

    '20221213-1621'

---
+++ 수업소스/test1213.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 13 16:20:02 2022
+@author: KITCOOP
+test1213.py
+"""
+
+'''
+1. chipotle.tsv 파일을 읽고 item 별 판매 갯수 시각화하기.
+   가장 많이 판매한 상품 10개만 막대그래프로 출력하기
+   20220701-2.png 참조
+'''   
+
+'''
+2. chipotle.tsv 파일을 읽고
+   Chicken Bowl을 2개 이상 주문한 주문 횟수 구하기
+   주문번호    Chicken Bowl 주문수량
+       1                 2
+       2                 3
+       3                 1
+    주문횟수 :   2    1,2,번주문만 횟수 
+'''
+
